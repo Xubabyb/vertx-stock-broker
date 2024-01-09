87a78614aae661fd7f63a22ac763ea07001d189b
Dmitriy Shidlovskiy <dshidlovskiy@t1-consulting.ru>
2024-01-09 20:23:35 +0300

    add lombok

--- src/main/java/org/dshid/vertx_stock_broker/dto/Asset.java
+++ src/main/java/org/dshid/vertx_stock_broker/dto/Asset.java
@@ -1,14 +1,10 @@
 package org.dshid.vertx_stock_broker.dto;
 
+import lombok.Value;
+
+@Value
 public class Asset {
 
-  private final String name;
+  String name;
 
-  public Asset(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
 }
